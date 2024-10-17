69e752a96758e6ba83bf6d0642594da6634aa961
iamAgbaCoder <demiladebamgboye@gmail.com>
2024-10-17 17:41:01 +0100

    Change DB

--- SalesAPI/SalesAPI/settings.py
+++ SalesAPI/SalesAPI/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'SalesAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600)
+# }
 
 
 SWAGGER_SETTINGS = {
